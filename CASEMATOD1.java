49627e7abc54d6cd623623339f36a4a0a183a679
adzrielllag-rgb <adzrielllag@gmail.com>
2025-10-08 15:09:22 +0700

    commit

---
+++ CASEMATOD1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class CASEMATOD1 { 
+    public static void main (String[] args) {
+
+      Scanner sc = new Scanner (System.in);
+
+      System.out.println("====INPUT DATA MAHASISWA====");
+      System.out.print("Nama : ");
+      String nama = sc.nextLine();
+      System.out.print("NIM  : ");
+      String nim = sc.nextLine();
+      
+      System.out.println();
+      System.out.println("--- Mata Kuliah 1: Algoritma dan Pemprogaman ---");
+      System.out.print("Nilai Tugas : ");
+      double tugas = sc.nextDouble();
+      System.out.print("Nilai UTS   : ");
+      double uts = sc.nextDouble();
+      System.out.print("Nilai UAS   : ");
+      double uas = sc.nextDouble();
+
+      
+     }
+    }
+
+
